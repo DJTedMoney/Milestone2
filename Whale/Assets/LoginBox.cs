0c658cfba617766fe9b2188db7a78eb6aaa7f9bf
Jason Heckard <jason.heckard@yahoo.com>
2014-04-22 22:43:09 -0700

    revisions to GUI

--- Whale/Assets/LoginBox.cs
+++ Whale/Assets/LoginBox.cs
@@ -3,21 +3,44 @@ using System.Collections;
 
 public class LoginBox : MonoBehaviour 
 {
-
+public string userName;
+	
+	public GUIText grafxText;
+	
+	public bool showLogin;
+	
 	// Use this for initialization
 	void Start () 
 	{
-	
+		// grafxText = GameObject.Find("GUIText").GetComponent<GUIText>();
+		
+		userName = "0000";
+		
+		// grafxText.text = "0000 0000";
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		grafxText.text = "hello";
 	}
 	
-	void OnGUI()
-	{
-		 GUI.Box(new Rect(0,0,1000, 1000),"This is not a title");
+	void OnGui()
+	{	
+		if(!showLogin)
+		{
+			if(GUI.Button (new Rect (100, 100, 100, 20), "Connect") )
+			{
+				grafxText.text = "Connect";
+			}
+		}
+		
+		else
+		{
+			if(GUI.Button (new Rect(10, 10, 100, 20), "Trying") )
+			{
+				grafxText.text = "Hello";
+			}
+		}
 	}
 }
