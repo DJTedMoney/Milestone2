b60738d4eb870d220885aff8528d104cb94017ab
Jason Heckard <jason.heckard@yahoo.com>
2014-04-23 01:37:52 -0700

    added recorded username and login button

--- Whale/Assets/Scripts/LoginBox.cs
+++ Whale/Assets/Scripts/LoginBox.cs
@@ -25,21 +25,31 @@ public string userName;
 		// grafxText.text = "hello";
 	}
 	
-	void OnGui()
+	void OnGUI()
 	{	
 		if(!showLogin)
 		{
-			if(GUI.Button (new Rect (100, 100, 100, 20), "Connect") )
+			GUI.Label(new Rect(130, 200, 100, 20), "UserName : ");
+			
+			userName = GUI.TextField(new Rect(200, 200, 100, 20), userName );
+			
+			if(GUI.Button (new Rect (200, 170, 100, 20), "Connect") )
 			{
-				grafxText.text = "Connect";
+				//grafxText.text = "Connect";
+				
+				showLogin = !showLogin;
 			}
 		}
 		
 		else
 		{
-			if(GUI.Button (new Rect(10, 10, 100, 20), "Trying") )
+			GUI.Label(new Rect(10, 40, 100, 20), userName);
+			
+			if(GUI.Button (new Rect(10, 10, 100, 20), "Disconnect") )
 			{
-				grafxText.text = "Hello";
+				//grafxText.text = "Hello";
+				
+				showLogin = !showLogin;
 			}
 		}
 	}
